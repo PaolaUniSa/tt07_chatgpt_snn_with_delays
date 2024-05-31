c67685a06417983de5800d7bcdf164d6e7333afb
Paola <105376831+PaolaUniSa@users.noreply.github.com>
2024-05-31 05:47:07 +0200

    Update InputCurrentCalculator.v

--- src/InputCurrentCalculator.v
+++ src/InputCurrentCalculator.v
@@ -1,17 +1,17 @@
 module InputCurrentCalculator #(
-    parameter M = 8                  // Number of input spikes and weights
+    parameter M = 24                  // Number of input spikes and weights
 )(
     input wire clk,                       // Clock signal
     input wire reset,                     // Asynchronous reset, active high
     input wire enable,                    // Enable input for calculation
     input wire [M-1:0] input_spikes,      // M-bit input spikes
     input wire [M*8-1:0] weights,         // M 8-bit weights
-    output reg [7:0] input_current   // 8-bit input current
+    output reg [7:0] input_current        // 8-bit input current
 );
     integer i;
     
     reg signed [7:0] weight_array [0:M-1];
-    reg signed [clog2(M*128)-1:0] current_sum;     // Adjusted bit-length for overflow handling
+    reg signed [clog2(M*128)-1:0] current_sum;  // Adjusted bit-length for overflow handling
 
     // Function to calculate the ceiling of log2
     function integer clog2;
@@ -35,6 +35,7 @@ module InputCurrentCalculator #(
     always @(posedge clk or posedge reset) begin
         if (reset) begin
             input_current <= 8'b0;
+            current_sum <= 0;  // Ensure current_sum is also reset
         end else if (enable) begin
             current_sum = 0;  // Initialize current sum to zero
             for (i = 0; i < M; i = i + 1) begin
