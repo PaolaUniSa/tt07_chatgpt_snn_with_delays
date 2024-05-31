128bc8506defbb768a518d91fdc48a1436d628a6
Paola <105376831+PaolaUniSa@users.noreply.github.com>
2024-05-31 14:43:26 +0200

    Update InputCurrentCalculator.v

--- src/InputCurrentCalculator.v
+++ src/InputCurrentCalculator.v
@@ -10,8 +10,8 @@ module InputCurrentCalculator #(
 );
     integer i;
     
-    reg signed [7:0] weight_array [0:M-1];
-    reg signed [13:0] current_sum;  // Adjusted bit-length for overflow handling [clog2(M*128)-1:0] current_sum;
+    reg signed [12:0] weight_array [0:M-1];//reg signed [7:0] weight_array [0:M-1];
+    reg signed [12:0] current_sum;  // Adjusted bit-length for overflow handling [clog2(M*128)-1:0] current_sum;
 
     // Function to calculate the ceiling of log2
 //    function integer clog2;
@@ -28,7 +28,8 @@ module InputCurrentCalculator #(
     // Convert the flattened weights array into a 2D array
     always @(*) begin
         for (i = 0; i < M; i = i + 1) begin
-            weight_array[i] = weights[i*8 +: 8];
+            weight_array[i][7:0] = weights[i*8 +: 8];
+            weight_array[i][12:8] = 5'b000000;
         end
     end
 
